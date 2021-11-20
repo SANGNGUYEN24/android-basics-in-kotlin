63d34ae5877a6a7d6ae6d455bee692e1b7e285c3
Sang Nguyen <sangnguyendinh24work@gmail.com>
2021-11-20 10:51:02 +0700

    Add input, output in BlurVieModel.kt, BlurWorker.kt

--- app/src/main/java/com/example/background/BlurViewModel.kt
+++ app/src/main/java/com/example/background/BlurViewModel.kt
@@ -22,21 +22,44 @@ import android.content.Context
 import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.background.workers.BlurWorker
 
 
 class BlurViewModel(application: Application) : ViewModel() {
 
     internal var imageUri: Uri? = null
     internal var outputUri: Uri? = null
+    private val workManager = WorkManager.getInstance(application)
 
     init {
         imageUri = getImageUri(application.applicationContext)
     }
+
+    /**
+     * Creates the input data bundle which includes the Uri to operate on
+     * @return Data which contains the Image Uri as a String
+     */
+    private fun createInputDataForUri(): Data {
+        val builder = Data.Builder()
+        imageUri?.let {
+            builder.putString(KEY_IMAGE_URI, imageUri.toString())
+        }
+        return builder.build()
+    }
+
     /**
      * Create the WorkRequest to apply the blur and save the resulting image
      * @param blurLevel The amount to blur the image
      */
-    internal fun applyBlur(blurLevel: Int) {}
+    internal fun applyBlur(blurLevel: Int) {
+        val blurRequest =
+            OneTimeWorkRequestBuilder<BlurWorker>().setInputData(createInputDataForUri()).build()
+        workManager.enqueue(blurRequest)
+    }
 
     private fun uriOrNull(uriString: String?): Uri? {
         return if (!uriString.isNullOrEmpty()) {
--- app/src/main/java/com/example/background/workers/BlurWorker.kt
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -2,27 +2,45 @@ package com.example.background.workers
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
+import android.text.TextUtils
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import com.example.background.R
+import androidx.work.workDataOf
+import com.example.background.KEY_IMAGE_URI
 
-private const val TAG = "BlutWorker"
-class BlurWorker (ctx: Context, params: WorkerParameters) : Worker(ctx, params){
+private const val TAG = "BlurWorker"
+
+class BlurWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
     override fun doWork(): Result {
         val appContext = applicationContext
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
         makeStatusNotification("Blurring image", appContext)
 
-        return try{
-            val picture = BitmapFactory.decodeResource(appContext.resources, R.drawable.android_cupcake)
+        return try {
+//            val picture = BitmapFactory.decodeResource(appContext.resources,
+//                R.drawable.android_cupcake
+//            )
+            if (TextUtils.isEmpty(resourceUri)) {
+                Log.e(TAG, "Invalid input Uri")
+                throw IllegalArgumentException("Invalid input Uri")
+            }
+
+            val resolver = appContext.contentResolver
+            val picture =
+                BitmapFactory.decodeStream(resolver.openInputStream(Uri.parse(resourceUri)))
+
             val output = blurBitmap(picture, appContext)
 
             // Write bitmap to a temp file
             val outputUri = writeBitmapToFile(appContext, output)
             makeStatusNotification("Output is $outputUri", appContext)
 
-            Result.success()
-        }catch (throwable: Throwable){
+            // Out data
+            val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+            Result.success(outputData)
+        } catch (throwable: Throwable) {
             Log.e(TAG, "Error applying blur")
             Result.failure()
 
